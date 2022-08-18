ac342ad333b83d8741c5527ac875d10bb76fd35d
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2022-08-18 12:18:00 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+const App = () => {
+  return (
+    <div className='container'>
+      <div>Name: App</div>
+      <div>Framework: react</div>
+      <div>Language: JavaScript</div>
+      <div>CSS: Empty CSS</div>
+    </div>
+  );
+};
+
+export default App;
